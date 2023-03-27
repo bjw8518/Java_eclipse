065582641381f988f88f30428ee9f557dd88ecb3
JinWoo <bjw8518@gmail.com>
2023-03-27 17:51:21 +0900

    java_HW
    java_HW

---
+++ HW/src/sorrt.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class sorrt {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		int ran =0;
+		
+		for(int i = 0; i<=100;i++) {
+			if(ran )
+		}
+
+}
