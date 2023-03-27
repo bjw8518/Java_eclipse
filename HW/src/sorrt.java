fa1079c175f86c8a622a62ab1944ea6d8ee455da
JinWoo <bjw8518@gmail.com>
2023-03-27 18:48:36 +0900

    commit
    commit

--- HW/src/sorrt.java
+++ HW/src/sorrt.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class sorrt {
 
 	public static void main(String[] args) {
-		Random r = new Random();
-		int ran =0;
 		
-		for(int i = 0; i<=100;i++) {
-			if(ran )
-		}
-
+		// 0부터 100사이의 임의의 수 30개 생성하여 ArrayList에 넣기
+		ArrayList<Integer>numbers=new ArrayList<>();
+		
+		Random Raa = new Random();
+		for (int i=0;1<30;i++) {
+			int NumList = Raa.nextInt(101);
+			numbers.add(NumList);}
+		int temp;
+	}
 }
---
+++ HW/src/sorrt2.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class sorrt2 {
+
+	public static void main(String[] args) {
+		// 0부터 100사이의 임의의 수 30개 생성하여 ArrayList에 넣기
+		Random RAA = new Random();
+		 int[] rray = new int[30];
+	}
+}
