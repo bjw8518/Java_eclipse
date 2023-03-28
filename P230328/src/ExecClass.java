47e515aa839079afbf46e4b619ce3834b223047a
JinWoo <bjw8518@gmail.com>
2023-03-28 09:08:13 +0900

    11
    111

---
+++ P230328/src/ExecClass.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class ExecClass {
+
+	public static void main(String[] args) {
+		int[] numbers = {0,0,0,0,0,0,0,0,0,0};
+		Random random = new Random();
+		for(int i =0;i<numbers.length;i++) {
+			
+		}
+	}
+
+}
