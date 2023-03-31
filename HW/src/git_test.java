b5c06684e8dc838966333915cbaebb1a147bb3dd
JinWoo <bjw8518@gmail.com>
2023-03-31 15:43:16 +0900

    ㅇㄴㅁㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇ

--- HW/src/git_test.java
+++ HW/src/git_test.java
@@ -2,9 +2,8 @@
 public class git_test {
 
 	public static void main(String[] args) {
-		System.out.println("git_test");
-		System.out.println("git_test");
-		System.out.println("git_test");
+		System.out.println("깃 테스트");
+		
 
 	}
 
