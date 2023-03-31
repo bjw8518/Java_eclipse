6395f627ba056d435fae199e16b8156348d0060b
JinWoo <bjw8518@gmail.com>
2023-03-31 10:22:30 +0900

    fac
    fac

---
+++ HW/src/fac.java
@@ -0,0 +1,18 @@
+
+public class fac {
+	
+	public static int factorial(int number) {
+		
+		int sum = 1;
+		for(int i=2;i<=number;i++) {
+			sum *=i;
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("10팩토리얼"+ factorial(10));
+
+	}
+
+}
